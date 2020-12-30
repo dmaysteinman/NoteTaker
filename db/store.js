cc2dd2f9a799b807d5666681d10ff8ab253ab501
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-12-29 19:00:26 -0500

    added code during tutoring session

--- db/store.js
+++ db/store.js
@@ -1,8 +1,8 @@
 const util = require("util");
 const fs = require("fs");
 
-// This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const uuidv1 = require("uuid/v1");
+// This package will be used to generate our unique ids.
+const uuidv1 = require("uuidv1");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
